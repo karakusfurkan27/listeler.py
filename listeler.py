300944699c9862b3203e3e8641bee76e33acc0f0
Furkan Karakuş <79601715+karakusfurkan27@users.noreply.github.com>
2024-12-23 11:19:25 +0300

    Add files via upload

---
+++ listeler.py
@@ -0,0 +1,31 @@
+# a= ["ahmet","mehmet","mustafa","ayse","fatma","mert"]
+# a.append("veysel")
+# a.remove("mehmet")
+# a.short()
+
+#print(a)
+
+
+a="python öğreniyorum"
+
+print(a[1])
+print(a[:2])
+print(a[2:])
+print(a[0:5])
+
+print(len(a))
+
+b=("ahmet","mehmet","mustafa","ayse","fatma","mert")
+
+print(type(b))
+print(b[1])
+
+sozluk ={"apple":"elma","car":"araba","train":"tren"}
+
+print(type(sozluk))
+
+print(sozluk["apple"])
+
+print(sozluk)
+
+sozluk["pen"]="kalem"
